73485813eb556332062f039343279c5537aedbbd
hamzajaada <hamzajaada22@gmail.com>
2024-01-10 01:55:44 +0100

    push

--- SOCKETAPP/src/Server/Server.java
+++ SOCKETAPP/src/Server/Server.java
@@ -1,3 +1,4 @@
+
 package Server;
 
 import java.io.IOException;
@@ -15,61 +16,60 @@ import Utils.UserDAO;
 import Utils.UserDAOImlp;
 
 public class Server {
-  public static void main(String[] args) throws ClassNotFoundException {
-    try {
-      ServerSocket serverSocket = new ServerSocket(1234);
-      System.out.println("Server waiting for client...");
-      while (true) {
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected");
-        ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            System.out.println("Server waiting for client...");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected");
 
-        // User receivedUser;
-        // receivedUser = (User) objectInputStream.readObject();
-        UserDAO userDAO = new UserDAOImlp();
-        CarDao carDAO = new CarDaoImpl();
-        Object receivedObject = objectInputStream.readObject(); // Méthode à implémenter
-        if (receivedObject instanceof User) {
-          userDAO.handleUserRequest((User) receivedObject);
-        } else if (receivedObject instanceof Car) {
-          carDAO.handleCarRequest((Car) receivedObject);
-        } else if ("getCarList".equals(receivedObject)) {
-          CarDao carDao = new CarDaoImpl();
-          List<Car> cars = carDao.getAllCars();
-          System.out.println(cars);
-          objectOutputStream.writeObject(cars);
+                // Créer une nouvelle instance de ClientHandler pour gérer la connexion client
+                Thread clientHandlerThread = new Thread(new ClientHandler(clientSocket));
+                clientHandlerThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        // UserDAO userDao = new UserDAOImlp();
-        // userDao.Ajouter(receivedUser);
-        // objectInputStream.close();
-        // clientSocket.close();
-        // User receivedUser = (User) objectInputStream.readObject();
-        // String action = receivedUser.getAction();
-        // UserDAO userDao = new UserDAOImlp();
+    }
+}
 
-        // switch (action) {
-        // case "register":
-        // userDao.Ajouter(receivedUser);
-        // System.out.println("User registered");
-        // break;
-        // case "login":
-        // boolean loginSuccess = userDao.login(receivedUser.getUsername(),
-        // receivedUser.getPassword());
-        // System.out.println("Login: " + (loginSuccess ? "Success" : "Failed"));
-        // break;
-        // default:
-        // System.out.println("Unknown action");
-        // break;
-        // }
-        // userDao.handleUserRequest(receivedUser);
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
 
-      }
-    } catch (IOException e) {
-
-      e.printStackTrace();
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
     }
 
-  }
+    @Override
+    public void run() {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 
+            // UserDAO et CarDao à utiliser dans ce thread
+            UserDAO userDAO = new UserDAOImlp();
+            CarDao carDAO = new CarDaoImpl();
+
+            Object receivedObject = objectInputStream.readObject();
+
+            if (receivedObject instanceof User) {
+                userDAO.handleUserRequest((User) receivedObject);
+            } else if (receivedObject instanceof Car) {
+                carDAO.handleCarRequest((Car) receivedObject);
+            } else if ("getCarList".equals(receivedObject)) {
+                List<Car> cars = carDAO.getAllCars();
+                System.out.println(cars);
+                objectOutputStream.writeObject(cars);
+            }
+
+            // Fermez les flux et la connexion client
+            objectInputStream.close();
+            objectOutputStream.close();
+            clientSocket.close();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
